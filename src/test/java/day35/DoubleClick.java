99e39065129a31c1076d080ea10b443ad4e43051
Sumit Gorai <goraisumit01@gmail.com>
2026-01-05 12:24:29 +0530

    double-click actions

--- src/test/java/day35/DoubleClick.java
+++ src/test/java/day35/DoubleClick.java
@@ -1,9 +1,48 @@
 package day35;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class DoubleClick {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://www.w3schools.com/TAgs/tryit.asp?filename=tryhtml5_ev_ondblclick3");
+		
+		driver.switchTo().frame("iframeResult");
+		
+		WebElement box1 = driver.findElement(By.xpath("//input[@id='field1']"));
+		WebElement box2 = driver.findElement(By.xpath("//input[@id='field2']"));
+		
+		
+		WebElement btn = driver.findElement(By.xpath("//button[normalize-space()='Copy Text']"));
+		
+		box1.clear();
+		box1.sendKeys("Welcome");
+		
+		// double click action on the button
+		Actions actions = new Actions(driver);
+		actions.doubleClick(btn).perform();
+		
+		//validation : box2 should be welcome
+//		String text = box2.getText();
+		String text = box2.getAttribute("value");
+
+		
+		if (text.equals("Welcome")) {
+			System.out.println("Text Copied");
+		}else {
+			System.out.println("Text not copied..");
+		}
+
 
 	}
 
